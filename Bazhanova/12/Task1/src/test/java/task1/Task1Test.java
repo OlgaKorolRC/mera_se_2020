3dc275d8b9949d3c8624fcdabc1e95e6686bea00
mila2592 <arksinus-bmw@yandex.ru>
2020-06-13 11:27:04 +0300

    Create Task1Test.java

---
+++ Bazhanova/12/Task1/src/test/java/task1/Task1Test.java
@@ -0,0 +1,21 @@
+package task1;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Task1Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
